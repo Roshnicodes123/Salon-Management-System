97b5851c3801c1f7a2b5b49ce11487553d11111f
Roshni Tiwari <rostiwari@bestpeers.in>
2024-03-04 18:47:25 +0530

    push with date nill issue

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -22,8 +22,10 @@ class Salon < ApplicationRecord
   
   
   
-  def available_seats(time_slot)
-    seat_capacity - appointments.where(time_slot: time_slot).count
+ def available_seats(time_slot)
+    total_capacity = seat_capacity
+    booked_appointments = appointments.where(time_slot: time_slot).count
+    available_seats = total_capacity - booked_appointments
+    [available_seats, 0].max
   end
- 
 end
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,11 +1,16 @@
 class TimeSlot < ApplicationRecord
   belongs_to :barbar
+  belongs_to :salon
 
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
+  validate :validate_seat_capacity, on: :create
 
   def formatted_start_time
     start_time.strftime("%Y-%m-%d %H:%M")
   end
-  # validates :available_seats, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  private
 
+  def validate_seat_capacity
+    errors.add(:base, 'Seat capacity for this time slot has been reached.') if appointments.count >= salon.seat_capacity
+  end
 end
